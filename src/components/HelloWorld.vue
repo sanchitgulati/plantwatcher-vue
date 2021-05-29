4d6f5be2ecee84e36c3806e2119b471fa27711af
Sanchit Gulati <sanchit.gulati@gmail.com>
2021-05-29 20:01:57 +0200

    Draft

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>{{ msg }}</h2>
+    <h1>{{data}}</h1>
   </div>
 </template>
 
@@ -35,6 +10,28 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  }, 
+  beforeMount(){
+    this.checkStatusSensor4();
+  },
+  data() {
+    return {
+      data: ""
+    }
+  },
+  methods: {
+    async checkStatusSensor4(){
+      const res = await fetch("https://jion-public.s3-us-west-2.amazonaws.com/sensor4.json",{
+        method:'GET',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json',
+        }
+      });
+      const data = await res.json();
+      this.data = data["sensor"] == false ? "No" : "Yes";
+      console.log(res);
+    }
   }
 }
 </script>
